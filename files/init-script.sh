da48f18a48f4f6b4ee8e74bbf3f1b71adcd61aa2
David Vavricka <vavricka.d@gmail.com>
2016-03-14 22:40:37 +0100

    upravy vol. IV.

--- files/init-script.sh
+++ files/init-script.sh
@@ -2,25 +2,31 @@
 
 # File:            init-script.sh
 # Version:         1.0
-# Last Changed:    Sat 5 Mar 2016 17:07:00 CET
+# Last Changed:    Mon 14 Mar 2016 22:23:00 CET
 # Author:          David Vavricka
 
 S_HDD_1="/mnt/hdd_1"
 S_RSYSD="$S_HDD_1/sbin/rsyslogd"
 S_RSYS_CONF="$S_HDD_1/rsyslog.conf"
 
+S_MAC_PREFIX='set $!macaddr="'
+I_MAC_NUM=$(sed 's/:/-/g' /sys/class/net/eth0/address)
+S_MAC="${S_MAC_PREFIX}${I_MAC_NUM}\";"
+
 killall klogd    && echo "klogd has been successfully killed"
 killall syslogd  && echo "syslogd has been successfully killed"
 killall rsyslogd && echo "rsyslogd has been successfully killed"
 
 #IF path to rsyslog libraries isn't set -> fix it
-if ! echo $(env | grep LD_LIBRARY_PATH) | grep -q "$S_HDD_1/lib"
+if ! echo $LD_LIBRARY_PATH | grep -q "$S_HDD_1/lib"
 then
     export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$S_HDD_1/lib"
 fi
 
-#Run rsyslog
+#Add Mac-addr of this STB to rsyslog.conf
+sed -i "/${S_MAC_PREFIX}/c\\${S_MAC}" "$S_RSYS_CONF"
 
+#Run rsyslog
 if [[ "$1" == "debug" ]];
 then
 	echo "starting rsyslog with debug output..."
--- files/log-rotate.sh
+++ files/log-rotate.sh
@@ -1,3 +1,36 @@
 #!/bin/sh
 
-mv -f /var/log/messages /var/log/messages.0
+# File:            log-rotate.sh
+# Version:         1.0
+# Last Changed:    Sat 12 Mar 2016 22:41:00 CET
+# Author:          David Vavricka
+
+NUM_BACKUPS="$1"
+
+LOG_NAME="/var/log/messages"
+
+if [ "$NUM_BACKUPS" -le "1" ]
+then
+    echo "$0: $1 is invalid argument, the number of backups must be greater than 1"
+exit
+fi
+
+#Increment postfix of all backup files
+num=$((NUM_BACKUPS))
+while [ $num -ge 0 ]; do
+
+    if [ -f "$LOG_NAME.$num" ]
+        then
+        mv -f "$LOG_NAME.$num" "$LOG_NAME.$((num+1))"
+    fi
+    num=$((num-1))
+done
+
+#If there is NUM_BACKUPS+1 files
+if [ -f "$LOG_NAME.$((NUM_BACKUPS+1))" ]
+then
+    #Delete the oldest one
+    rm -f "$LOG_NAME.$((NUM_BACKUPS+1))"
+fi
+
+mv -f $LOG_NAME $LOG_NAME.0
