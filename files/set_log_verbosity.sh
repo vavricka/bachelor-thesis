908ecaea6081d75945afc71cd4be5d2024203758
David Vavricka <vavricka.d@gmail.com>
2016-09-05 13:29:11 +0200

    set_log_verbosity.sh can add line with new component

--- files/set_log_verbosity.sh
+++ files/set_log_verbosity.sh
@@ -33,16 +33,37 @@ for ARG in "$@"
         	exit 1
     	fi
 
-    	#if $programname == 'solid' and $syslogseverity-text > 'info'
+    	#if $programname == 'solid' and $syslogseverity > 7
     	S_COMPL_LINE="${S_PREFIX}'${S_PROG_NAME}'${S_MID}'$ARG'"
 
-    	#If given APP-NAME is inside rsyslog.conf
+    	#If given APP-NAME is inside rsyslog.conf -> change the line with given app-anme
     	if grep -q "${S_PREFIX}'${S_PROG_NAME}'${S_MID}" "$S_RSYS_CONF"
     	then
         	I_LINE_NUM=$(grep -n "${S_PREFIX}'${S_PROG_NAME}'${S_MID}" "$S_RSYS_CONF" | head -1 | cut -d : -f 1)
 
 			(head -$(($I_LINE_NUM-1)) "$S_RSYS_CONF"; echo "$S_COMPL_LINE"; \
 				tail -n +$(($I_LINE_NUM+1)) "$S_RSYS_CONF") > TMP_FILE && mv TMP_FILE "$S_RSYS_CONF";
+		else
+		#create new line	
+			if grep -q "#LOG_VERBOSITY_SETUP" "$S_RSYS_CONF"
+			then
+				I_LOG_VER_LINE=$(grep -n "#LOG_VERBOSITY_SETUP" "$S_RSYS_CONF" | head -1 | cut -d : -f 1)
+			else
+				echo "$0: Missing #LOG_VERBOSITY_SETUP line."
+			fi	
+			
+			head -n $((I_LOG_VER_LINE)) $S_RSYS_CONF > TMP_FILE
+
+			I_LINES=$(cat $S_RSYS_CONF | wc -l)
+			I_LINES=$((I_LINES-I_LOG_VER_LINE+1))
+
+			echo "$S_COMPL_LINE" >> TMP_FILE
+			echo "then { stop }" >> TMP_FILE
+			echo "" >> TMP_FILE
+			
+			tail -n $I_LINES $S_RSYS_CONF >> TMP_FILE
+			cat TMP_FILE > $S_RSYS_CONF
+			rm -f TMP_FILE
 		fi
 
     else
